e5e92fee9a20a786345b266e258363458ca15c2f
MaramNaqeeb <maramnaqib1998@gmail.com>
2022-10-24 13:11:27 +0300

    first

---
+++ Java fundamentals assignments/first_assignment/Information.java
@@ -0,0 +1,15 @@
+public class Information{
+    public static void main(String[]args){
+    
+        System.out.println(" I am Maram Naqib");
+    
+
+    
+        System.out.println("I am 24 years old");
+    
+
+    
+        System.out.println(" My hometown is Palestin, Nablus");
+    }
+    }
+
