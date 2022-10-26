d9c4080cd3b66c02302b97e9a7b986681180be82
MaramNaqeeb <maramnaqib1998@gmail.com>
2022-10-26 16:52:12 +0300

    order items

---
+++ Java fundamentals assignments/order items/Order.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Order{
+    String name;
+    double total;
+    boolean ready;
+
+
+    ArrayList<Item> myItem=new ArrayList<Item>(){
+
+
+    };
+
+
+}
+
+
