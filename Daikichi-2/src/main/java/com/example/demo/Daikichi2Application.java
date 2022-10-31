d149ce2f882fdb5fe06123f3d65b271faa2d2717
MaramNaqeeb <maramnaqib1998@gmail.com>
2022-10-31 14:25:13 +0200

    travel

---
+++ Daikichi-2/src/main/java/com/example/demo/Daikichi2Application.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+
+public class Daikichi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Daikichi2Application.class, args);
+	
+	}
+
+}
