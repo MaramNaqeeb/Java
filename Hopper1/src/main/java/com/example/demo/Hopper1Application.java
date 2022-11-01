d98aaf018f507adae4e7645f20aa19d695eb731f
MaramNaqeeb <maramnaqib1998@gmail.com>
2022-11-01 14:26:12 +0200

    hopper1

---
+++ Hopper1/src/main/java/com/example/demo/Hopper1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hopper1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hopper1Application.class, args);
+	}
+
+}
