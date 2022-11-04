304d3783b7659178125b2ebf9ed48dd36c01aa4c
MaramNaqeeb <maramnaqib1998@gmail.com>
2022-11-04 22:26:44 +0200

    render

--- Book/src/main/java/com/example/demo/controllers/BookController.java
+++ Book/src/main/java/com/example/demo/controllers/BookController.java
@@ -1,46 +1,32 @@
-//package com.example.demo.controllers;
-//
-//import javax.servlet.http.HttpSession;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//
-//import com.example.demo.models.Book;
-//
-//
-//
-//@Controller
-//
-//public class BookController {
-//	private Object bookService;
-//
-//	@GetMapping({"books"})
-//	public String form() {
-//		return "form.jsp";
-//	}
-//
-//	@PostMapping("/register")
-//	public String register(@RequestParam(value = "title") String title, @RequestParam(value = "description") String description,
-//			@RequestParam(value = "language") String language, @RequestParam(value = "pages") Integer pages,
-//			
-//			HttpSession session) {
-//		session.setAttribute("title", title);
-//		session.setAttribute("description", description);
-//		session.setAttribute("language", language);
-//		session.setAttribute("pages", pages);
-//
-//		return "redirect:/show";
-//
-//	}
-//
-//	@GetMapping("/books/{bookId}")
-//	public String findOwner(@PathVariable String bookId, Model model) {
-//		 Book book = bookService.findBook(bookId);  
-//		  model.addAttribute("book", book);  
-//		  return "displayOwner"; 
-//		
-//	}
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.example.demo.models.Book;
+import com.example.demo.services.BookService;
+
+
+
+@Controller
+
+public class BookController {
+
+	
+	   private BookService bookService;
+	    public BookController(BookService bookService){
+	        this.bookService = bookService;
+	    }
+
+	@GetMapping("/books/{id}")
+	
+	public String index( Model model, @PathVariable ("id") Long id) {
+		Book book = bookService.findBook(id); 
+		  model.addAttribute("book", book);  
+		 
+		  return "index.jsp"; 
+	
+	}
+}
